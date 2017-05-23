b5dafe2b475f83674866eb6dba2ee9dba3770942
Emil Kottmeir <emil.kottmeir@googlemail.com>
2017-05-23 18:21:35 +0200

    payment recieved hook, generate and save keys

--- wp-content/themes/team-manager/functions.php
+++ wp-content/themes/team-manager/functions.php
@@ -6,6 +6,8 @@
  * Time: 14:39
  */
 
+
+
 function tm_child_styles() {
     wp_deregister_style( 'parallax-one-style');
     wp_register_style('parallax-one-style', get_template_directory_uri(). '/style.css');
@@ -20,3 +22,85 @@ function kb_svg ( $svg_mime ){
 }
 
 add_filter( 'upload_mimes', 'kb_svg' );
+
+
+
+
+/*-------------- generate keys -------------------------*/
+
+function generate_key() {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $key_strings = array();
+    $key = array();
+    for ($a = 0; $a < 5; $a++) {
+        $string = array();
+        for ($i = 0; $i < 5; $i++) {
+            array_push($string, $characters[rand(0, $charactersLength - 1)]);
+        }
+        array_push($key_strings, $string);
+        unset($string);
+    }
+    foreach ($key_strings as $string_item){
+        array_push($key, implode( '', $string_item ));
+    }
+    return implode( '-', $key );
+}
+
+function save_license($license){
+    $servername = "localhost";
+    $username = "tm";
+    $password = "tmtest#9";
+    $dbname = "licence_db";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $customer = array_column($license, 'customer');
+    $first_name = array_column($customer, 'first_name');
+    $last_name = array_column($customer, 'last_name');
+    $email = array_column($customer, 'email');
+    $license_type = array_column($license, 'product');
+    $license_key = array_column($license, 'key');
+
+
+    $sql = "INSERT INTO tm_licences (first_name, last_name, email, license_type, license_key)
+        VALUES ('$first_name[0]' , '$last_name[0]', '$email[0]', '$license_type[0]', '$license_key[0]')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+
+
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+
+function process_products ($order_id) {
+
+    if (did_action('woocommerce_thankyou') === 1) {
+
+        $order = new WC_Order($order_id);
+        $customer_first_name = $order->get_billing_first_name();
+        $customer_last_name = $order->get_billing_last_name();
+        $customer_email = $order->get_billing_email();
+        $order_items = $order->get_items();
+        $customer = array('first_name' => $customer_first_name, 'last_name' => $customer_last_name, 'email' => $customer_email);
+
+        foreach ($order_items as $product) {
+            echo $product;
+            $license[] = array('product' => $product['name'], 'key' => generate_key(), 'customer' => $customer);
+            save_license($license);
+            unset($license);
+        }
+    }
+}
+
+add_action('woocommerce_thankyou', 'process_products', 10);
