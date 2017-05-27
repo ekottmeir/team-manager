5570ce3522ec24c3ca43e1da7d7de3fd899dab2e
Emil Kottmeir <emil.kottmeir@googlemail.com>
2017-05-27 10:40:26 +0200

    save license key and send mail

--- wp-content/themes/team-manager/functions.php
+++ wp-content/themes/team-manager/functions.php
@@ -7,6 +7,7 @@
  */
 
 
+/*-------------- register child theme -------------------------*/
 
 function tm_child_styles() {
     wp_deregister_style( 'parallax-one-style');
@@ -14,17 +15,13 @@ function tm_child_styles() {
     wp_enqueue_style('parallax-one-style', get_template_directory_uri(). '/style.css');
     wp_enqueue_style( 'tm-style', get_stylesheet_directory_uri().'/style.css', array('parallax-one-style') );
 }
-add_action( 'wp_enqueue_scripts', 'tm_child_styles' );
+
+/*-------------- set upload types -------------------------*/
 
 function kb_svg ( $svg_mime ){
     $svg_mime['svg'] = 'image/svg+xml';
     return $svg_mime;
 }
-
-add_filter( 'upload_mimes', 'kb_svg' );
-
-
-
 
 /*-------------- generate keys -------------------------*/
 
@@ -47,11 +44,24 @@ function generate_key() {
     return implode( '-', $key );
 }
 
-function save_license($license){
+/*-------------- save license to db -------------------------*/
+
+
+
+function save_license($license, $order_id, $product){
+/*
+    //live
+    $servername = "localhost";
+    $username = "teamManagerAdmin";
+    $password = "teamManagerAdmin2017#!";
+    $dbname = "teamManagerUsers";
+*/
+
+     //local
     $servername = "localhost";
     $username = "tm";
     $password = "tmtest#9";
-    $dbname = "licence_db";
+    $dbname = "teamManagerUsers";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
@@ -61,46 +71,133 @@ function save_license($license){
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $customer = array_column($license, 'customer');
-    $first_name = array_column($customer, 'first_name');
-    $last_name = array_column($customer, 'last_name');
-    $email = array_column($customer, 'email');
-    $license_type = array_column($license, 'product');
-    $license_key = array_column($license, 'key');
+    $order = new WC_Order($order_id);
 
+    $id = '';
+    $first_name = $order->get_billing_first_name();
+    $_last_name = $order->get_billing_last_name();
+    $club = $order->get_billing_company();
+    $date = '';
+    $units = '';
+    $product_type = $product['name'];
+    $accountname = $club . ' - ' . $first_name . ' ' . $_last_name;
 
-    $sql = "INSERT INTO tm_licences (first_name, last_name, email, license_type, license_key)
-        VALUES ('$first_name[0]' , '$last_name[0]', '$email[0]', '$license_type[0]', '$license_key[0]')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-
-
+    if($product_type == 'Einzellizenz' || $product_type == 'Vereinslizenz') {
+        $date = '01.01.2000';
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        $date = date('d.m.Y', strtotime('+1 year'));
+    }
+
+    if($product_type == 'Einzellizenz' || $product_type == 'Einzellizenz Miete') {
+        $units = '2';
+    } else {
+        $units = '6';
+    }
+
+    $sql = 'SELECT MAX(id) FROM accounts';
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $id = $row['MAX(id)'];
+            $new_id = $id + 1;
+
+            $sql2 = "INSERT INTO accounts (id, accountname, accounttype, expirydate, licensenumber, units)
+            VALUES ('$new_id','$accountname' , '1', '$date', '$license', '$units')";
+
+            if ($conn->query($sql2) === TRUE) {
+                echo "New record created successfully";
+
+            } else {
+                echo "Error: " . $sql2 . "<br>" . $conn->error;
+            }
+        }
+    } else {
+        echo "0 results";
     }
 
     $conn->close();
 }
+
+    /*-------------- get order data -------------------------*/
 
 function process_products ($order_id) {
 
     if (did_action('woocommerce_thankyou') === 1) {
 
         $order = new WC_Order($order_id);
-        $customer_first_name = $order->get_billing_first_name();
-        $customer_last_name = $order->get_billing_last_name();
-        $customer_email = $order->get_billing_email();
         $order_items = $order->get_items();
-        $customer = array('first_name' => $customer_first_name, 'last_name' => $customer_last_name, 'email' => $customer_email);
-
+        $order_licenses = array();
         foreach ($order_items as $product) {
-            echo $product;
-            $license[] = array('product' => $product['name'], 'key' => generate_key(), 'customer' => $customer);
-            save_license($license);
-            unset($license);
+            $license_key = generate_key();
+            $license = array('name' => $product['name'], 'key' => $license_key);
+            save_license($license_key, $order_id, $product);
+            array_push($order_licenses, $license);
         }
+        send_license_mail($order_licenses, $order->get_billing_email());
     }
 }
 
+function send_license_mail($order_licenses, $email) {
+
+    $rows = '';
+
+    foreach ($order_licenses as $product) {
+        $rows .= '<tr><td>' . $product['name'] . '</td><td>' . $product['key'] . '</td></tr>';
+    }
+
+    $to = $email;
+    $subject = 'Handball Team Manager';
+    $headers = "From: " . strip_tags($_POST['req-email']) . "\r\n";
+    $headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    $content = "
+    <html>
+        <head>
+            <style>
+                p {
+                    font-size: 15px;
+                    margin-bottom: 40px;
+                    margin-top: 30px;
+                }
+
+                table, th, td {
+                    border: 2px solid #eee;
+                    border-collapse: collapse;
+                }
+
+                th, td {
+                    padding: 10px 20px;
+                    font-size: 15px;
+                }
+
+                th {
+                    text-align: left;
+                    background-color: #EEEEEE;
+                }
+            </style>
+        </head>
+        <body>
+            <h2>Lizenzschlüssel</h2>
+            <p>Diese Email enthält Lizenzschlüssel für die Aktivierung Ihrer Software.</p>
+            <table>
+                <tr>
+                    <th>Lizenz</th>
+                    <th>Key</th>
+                </tr>
+                {$rows}
+            </table>
+        </body>
+    </html>
+    ";
+
+
+    wp_mail($to, $subject, $content, $headers);
+}
+
+add_action( 'wp_enqueue_scripts', 'tm_child_styles' );
+add_filter( 'upload_mimes', 'kb_svg' );
 add_action('woocommerce_thankyou', 'process_products', 10);
